8decee57115241040dab4e6ef410d62f2265ed2d
Gamby <spmtaebin@cau.ac.kr>
2023-10-26 00:53:32 +0900

    Add files via upload

---
+++ bouncingBall_Script.py
@@ -0,0 +1,38 @@
+import maya.cmds as cmds
+
+initial_height = 3 #m
+initial_velocity = -5.0 #m/s
+gravity = 9.806 #m/s^2
+restitution_coefficient = 0.8
+fps = 24
+posY = []
+
+def freeFall () :
+    current_height = initial_height
+    current_velocity = initial_velocity
+    time = 0
+    while current_height >= 0:
+        posY.append(current_height)
+        current_height = initial_height + initial_velocity * time- 0.5 * gravity * time * time
+        current_velocity = initial_velocity - gravity * time
+        time += 1/fps
+    return verticalUpward (current_velocity, 1)
+
+def verticalUpward (current_velocity, bounced):
+    if (bounced >= 10) : return
+    time = 0
+    current_height = 0
+    initial_velocity = -1 * current_velocity * restitution_coefficient
+    current_velocity = initial_velocity
+    while current_height >= 0:
+        posY.append(current_height)
+        current_height = initial_velocity * time - 0.5 * gravity * time * time
+        current_velocity = initial_velocity - gravity * time
+        time += 1/fps
+    return verticalUpward(current_velocity, bounced+1)
+
+freeFall ()
+
+
+for i in range (239):
+    cmds.setKeyframe('all_CON.translateY', value=posY[i], time=i+1)
